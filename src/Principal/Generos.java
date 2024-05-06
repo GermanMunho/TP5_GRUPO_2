76f052d040fb1d7a1af3c601b818a258a989b13f
willian-alv <44449425+willian-alv@users.noreply.github.com>
2024-05-05 21:25:11 -0300

    Add files via upload

---
+++ src/Principal/Generos.java
@@ -0,0 +1,23 @@
+package Principal;
+
+public class Generos {
+	private String nombre;
+	
+	public Generos(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return nombre;
+	}
+	
+}
--- src/Principal/Pelicula.java
+++ src/Principal/Pelicula.java
@@ -1,5 +1,35 @@
 package Principal;
 
 public class Pelicula {
-
+	
+	private String id;
+	private String nombre;
+	private Generos genero;
+	private static int cont = 0;
+	
+	public Pelicula() {
+		cont++;
+		id = Integer.toString(cont);
+		genero.setNombre("Sin genero");
+	}
+	public Pelicula(String nombre, String genero) {
+		cont++;
+		id = Integer.toString(cont);
+		this.nombre = nombre;
+		this.genero = new Generos(genero);
+	}
+	public String getId() {
+		return id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return id + "- " + nombre + " - Genero: " + genero.toString();
+	}
+	
 }
